762fd2056c6833d25732d560899c493af14c315b
Sebastian Stasica <s.stasica@o2.pl>
2020-10-11 19:58:59 +0200

    Create index.php

---
+++ dook/index.php
@@ -0,0 +1,18 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+$files = glob('../images/*');
+if ($files) {
+    $FERRET_NUM = count($files);
+    $random_ferret_index = rand(1, $FERRET_NUM);
+
+    $image_path = 'http://randomferret.site/images/'.$random_ferret_index.'.jpg';
+    $link = 'http://randomferret.fun/?i='.$random_ferret_index;
+} else {
+    $image_path = null;
+    $link = null;
+}
+$data = ['image' => $image_path, 'link' => $link];
+
+echo json_encode($data);
